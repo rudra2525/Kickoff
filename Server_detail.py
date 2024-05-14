22629511a82e47d2947dddb88df3f714eba6597a
rudra2525 <34281605+rudra2525@users.noreply.github.com>
2024-05-14 10:58:15 -0500

    Update Server_detail.py

--- Server_detail.py
+++ Server_detail.py
@@ -4,18 +4,19 @@ import menu
 import time
 
 def setup_server_details():
+    # Fetch data from database
     Os_type_option = myDbConnection.query_os_type()
     Instance_type_option = myDbConnection.query_allowed_values('AWS_INSTANCE')
 
     adGrprows = []
 
     AdGrpTableCol = [
-        {'name': 'Os_type', 'label': 'OS Type', 'field': 'Os_type', 'required': True, 'align': 'left', 'width': '20%', 'headerStyle': 'min-width: 25px'},
-        {'name': 'instance_type', 'label': 'Instance Type', 'field': 'instance_type', 'required': True, 'align': 'left', 'width': '20%', 'headerStyle': 'min-width: 25px'},
-        {'name': 'server_prefix', 'label': 'Server Prefix', 'field': 'server_prefix', 'required': True, 'align': 'left', 'width': '20%', 'headerStyle': 'min-width: 25px'},
-        {'name': 'Server_sequence_number', 'label': 'Server Sequence Number', 'field': 'Server_sequence_number', 'required': True, 'align': 'left', 'width': '20%', 'headerStyle': 'min-width: 25px'},
-        {'name': 'Quantity', 'label': 'Quantity', 'field': 'Quantity', 'required': True, 'align': 'left', 'width': '20%', 'headerStyle': 'min-width: 25px'},
-        {'name': 'Subnet_information', 'label': 'Subnet Information', 'field': 'Subnet_information', 'required': True, 'align': 'left', 'width': '20%', 'headerStyle': 'min-width: 25px'},
+        {'name': 'Os_type', 'label': 'OS Type', 'field': 'Os_type', 'required': True, 'align': 'left', 'width': '20%'},
+        {'name': 'instance_type', 'label': 'Instance Type', 'field': 'instance_type', 'required': True, 'align': 'left', 'width': '20%'},
+        {'name': 'server_prefix', 'label': 'Server Prefix', 'field': 'server_prefix', 'required': True, 'align': 'left', 'width': '20%'},
+        {'name': 'Server_sequence_number', 'label': 'Server Sequence Number', 'field': 'Server_sequence_number', 'required': True, 'align': 'left', 'width': '20%'},
+        {'name': 'Quantity', 'label': 'Quantity', 'field': 'Quantity', 'required': True, 'align': 'left', 'width': '20%'},
+        {'name': 'Subnet_information', 'label': 'Subnet Information', 'field': 'Subnet_information', 'required': True, 'align': 'left', 'width': '20%'},
     ]
 
     def deleteGrp(e: events.GenericEventArguments) -> None:
@@ -29,53 +30,61 @@ def setup_server_details():
         for row in adGrprows:
             print(row)
 
-
     with ui.card().classes('w-full'):
-            with ui.table(columns=AdGrpTableCol, rows=adGrprows).classes('w-full') as aGrptable:
-                aGrptable.add_slot(
-                    "body",
-                    r"""
-                    <q-tr :props="props">
-                        <q-td key="Os_type" :props="props" class="w-8 ellipsis">
-                            {{ props.row.Os_type }}
-                        </q-td>
-                        <q-td key="instance_type" :props="props" class="w-8 ellipsis">
-                            {{ props.row.instance_type }}
-                        </q-td>
-                        <q-td key="server_prefix" :props="props" class="w-8 ellipsis">
-                            {{ props.row.server_prefix }}
-                        </q-td>
-                        <q-td key="Server_sequence_number" :props="props" class="w-8 ellipsis">
-                            {{ props.row.Server_sequence_number }}
-                        </q-td>
-                        <q-td key="Quantity" :props="props" class="w-8 ellipsis">
-                            {{ props.row.Quantity }}
-                        </q-td>
-                        <q-td key="Subnet_information" :props="props" class="w-8 ellipsis">
-                            {{ props.row.Subnet_information }}
-                        </q-td>
-                        <q-td auto-width>
-                            <q-btn size="sm" color="warning" round dense icon="delete" :props="props"
-                                @click="() => $parent.$emit('delete', props.row)">
-                            </q-btn>
-                        </q-td>
-                    </q-tr>
-                    """,
-                )
-                with aGrptable.add_slot('bottom-row'):
-                    with aGrptable.row():
-                        with aGrptable.cell():
-                            u_env_type = ui.select(options=Os_type_option, with_input=True, value="", label='OS Type').classes(f"w-full").props('rounded outlined dense')
-                        with aGrptable.cell():
-                            u_aws_acc = menu.form_select(Instance_type_option, 'Instance Type', "", "w-full").props('rounded outlined dense')
-                        with aGrptable.cell():
-                            u_aws_reg = ui.input(label="Server Prefix", placeholder="Enter Server Prefix").classes('w-full').props('rounded outlined dense''clearable')
-                        with aGrptable.cell():
-                            ui.button(on_click=lambda: (aGrptable.add_rows({'id': time.time(), 'Environment_type': u_env_type.value,'aws_account': u_aws_acc.value, "aws_region": u_aws_reg.value}),
-                                u_env_type.set_value(None),
-                                u_aws_acc.set_value(None),
-                                u_aws_reg.set_value(None)
-                            ), icon='add')
+        with ui.table(columns=AdGrpTableCol, rows=adGrprows).classes('w-full') as aGrptable:
+            aGrptable.add_slot(
+                "body",
+                r"""
+                <q-tr :props="props">
+                    <q-td key="Os_type" :props="props" class="w-8 ellipsis">
+                        {{ props.row.Os_type }}
+                    </q-td>
+                    <q-td key="instance_type" :props="props" class="w-8 ellipsis">
+                        {{ props.row.instance_type }}
+                    </q-td>
+                    <q-td key="server_prefix" :props="props" class="w-8 ellipsis">
+                        {{ props.row.server_prefix }}
+                    </q-td>
+                    <q-td key="Server_sequence_number" :props="props" class="w-8 ellipsis">
+                        {{ props.row.Server_sequence_number }}
+                    </q-td>
+                    <q-td key="Quantity" :props="props" class="w-8 ellipsis">
+                        {{ props.row.Quantity }}
+                    </q-td>
+                    <q-td key="Subnet_information" :props="props" class="w-8 ellipsis">
+                        {{ props.row.Subnet_information }}
+                    </q-td>
+                    <q-td auto-width>
+                        <q-btn size="sm" color="warning" round dense icon="delete" :props="props"
+                            @click="() => $parent.$emit('delete', props.row)">
+                        </q-btn>
+                    </q-td>
+                </q-tr>
+                """,
+            )
+            with aGrptable.add_slot('bottom-row'):
+                with aGrptable.row():
+                    with aGrptable.cell():
+                        os_type_select = ui.select(options=Os_type_option, label='OS Type')
+                    with aGrptable.cell():
+                        instance_type_select = ui.select(options=Instance_type_option, label='Instance Type')
+                    with aGrptable.cell():
+                        server_prefix_input = ui.input(label='Server Prefix')
+                    with aGrptable.cell():
+                        server_seq_num_input = ui.input(label='Server Sequence Number')
+                    with aGrptable.cell():
+                        quantity_input = ui.input(label='Quantity')
+                    with aGrptable.cell():
+                        subnet_info_input = ui.input(label='Subnet Information')
+                    with aGrptable.cell():
+                        ui.button(on_click=lambda: (aGrptable.add_rows({'id': time.time(), 'Os_type': os_type_select.value, 'instance_type': instance_type_select.value, 'server_prefix': server_prefix_input.value, 'Server_sequence_number': server_seq_num_input.value, 'Quantity': quantity_input.value, 'Subnet_information': subnet_info_input.value}),
+                                  os_type_select.set_value(None),
+                                  instance_type_select.set_value(None),
+                                  server_prefix_input.set_value(None),
+                                  server_seq_num_input.set_value(None),
+                                  quantity_input.set_value(None),
+                                  subnet_info_input.set_value(None)
+                                                    ), icon='add')
+                aGrptable.on("delete", deleteGrp)
 
-                    aGrptable.on("delete", deleteGrp)
-            ui.button('Save', on_click=print_table_data, icon='save')
+        ui.button('Save', on_click=print_table_data, icon='save')
