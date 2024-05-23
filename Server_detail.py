8c1900b488956efe88d3fd7c38f4c6b6dfe6b9a4
rudra2525 <34281605+rudra2525@users.noreply.github.com>
2024-05-23 09:51:07 -0500

    Update Server_detail.py

--- Server_detail.py
+++ Server_detail.py
@@ -1,90 +1,106 @@
+import asyncio
 from nicegui import events, ui
 import myDbConnection
 import menu
 import time
+from website.demo import bash_window
 
-def setup_server_details():
+
+def setup_server_details(on_saverow: callable):
     # Fetch data from database
     Os_type_option = myDbConnection.query_os_type()
     Instance_type_option = myDbConnection.query_allowed_values('AWS_INSTANCE')
 
-    adGrprows = []
+    server_details_rows = []
 
-    AdGrpTableCol = [
-        {'name': 'Os_type', 'label': 'OS Type', 'field': 'Os_type', 'required': True, 'align': 'left', 'width': '20%'},
-        {'name': 'instance_type', 'label': 'Instance Type', 'field': 'instance_type', 'required': True, 'align': 'left', 'width': '20%'},
-        {'name': 'server_prefix', 'label': 'Server Prefix', 'field': 'server_prefix', 'required': True, 'align': 'left', 'width': '20%'},
-        {'name': 'Server_sequence_number', 'label': 'Server Sequence Number', 'field': 'Server_sequence_number', 'required': True, 'align': 'left', 'width': '20%'},
-        {'name': 'Quantity', 'label': 'Quantity', 'field': 'Quantity', 'required': True, 'align': 'left', 'width': '20%'},
-        {'name': 'Subnet_information', 'label': 'Subnet Information', 'field': 'Subnet_information', 'required': True, 'align': 'left', 'width': '20%'},
+    def save_server_details():
+        server_details = {
+            'id': time.time(),  # Unique ID for each row
+            'Os_type': os_type_select.value,
+            'instance_type': instance_type_select.value,
+            'server_prefix': server_prefix_input.value,
+            'Server_sequence_number': server_seq_num_input.value,
+            'Quantity': quantity_input.value,
+            'Subnet_information': subnet_info_input.value
+        }
+        server_details_rows.append(server_details)
+        serverDetailsTable.options['rowData'] = server_details_rows
+        serverDetailsTable.update()
+        serverDetailsDialog.close()
+
+    async def edit_server_details():
+        selected_row = await serverDetailsTable.get_selected_row()
+        if selected_row:
+            for i, row in enumerate(server_details_rows):
+                if row['id'] == selected_row['id']:
+                    server_details_rows[i] = {
+                        'id': row['id'],
+                        'Os_type': os_type_select.value,
+                        'instance_type': instance_type_select.value,
+                        'server_prefix': server_prefix_input.value,
+                        'Server_sequence_number': server_seq_num_input.value,
+                        'Quantity': quantity_input.value,
+                        'Subnet_information': subnet_info_input.value
+                    }
+                    break
+            serverDetailsTable.options['rowData'] = server_details_rows
+            serverDetailsTable.update()
+            serverDetailsDialog.close()
+        else:
+            ui.notify("No row selected for editing", type='warning')
+
+    async def remove_server_details():
+        selected_row = await serverDetailsTable.get_selected_row()
+        if selected_row:
+            server_details_rows[:] = [row for row in server_details_rows if row['id'] != selected_row['id']]
+            serverDetailsTable.options['rowData'] = server_details_rows
+            serverDetailsTable.update()
+            ui.notify("Row removed successfully", type='positive')
+        else:
+            ui.notify("No row selected for removal", type='warning')
+
+    def save_all_details():
+        print("Server Details:")
+        for row in server_details_rows:
+            print(row)
+        ui.notify("Data Saved", type='positive')
+
+    with ui.dialog() as serverDetailsDialog:
+        with bash_window("Add Server Detail", classes='w-full'):
+            with ui.grid(columns=2).classes('gap-4'):
+                os_type_select = ui.select(options=Os_type_option, label='OS Type').classes('pl-6 w-5/6')
+                instance_type_select = ui.select(options=Instance_type_option, with_input=True, value="",
+                                                 label='Instance Type').classes('pl-6 w-5/6')
+                server_prefix_input = ui.input(label='Server Prefix').classes('pl-6 w-5/6')
+                server_seq_num_input = ui.input(label='Server Sequence Number').classes('pl-6 w-5/6')
+                quantity_input = ui.input(label='Quantity').classes('pl-6 w-5/6')
+                subnet_info_input = ui.input(label='Subnet Information').classes('pl-6 w-5/6')
+
+            with ui.row().classes('self-center place-items-center'):
+                ui.button('Add', on_click=save_server_details, icon='add').classes('m-2')
+                ui.button('Close', on_click=serverDetailsDialog.close, icon='close').classes('m-2')
+
+    server_details_columns = [
+        {'headerName': '', 'checkboxSelection': True, 'width': 50},  # Add checkbox selection
+        {'headerName': 'OS Type', 'field': 'Os_type'},
+        {'headerName': 'Instance Type', 'field': 'instance_type'},
+        {'headerName': 'Server Prefix', 'field': 'server_prefix'},
+        {'headerName': 'Server Sequence Number', 'field': 'Server_sequence_number'},
+        {'headerName': 'Quantity', 'field': 'Quantity'},
+        {'headerName': 'Subnet Information', 'field': 'Subnet_information'}
     ]
 
-    def deleteGrp(e: events.GenericEventArguments) -> None:
-        nonlocal adGrprows
-        adGrprows[:] = [row for row in adGrprows if row["id"] != e.args["id"]]
-        ui.notify(f"Delete {e.args['id']}")
-        aGrptable.update()
-
-    def print_table_data():
-        print("Table Data:")
-        for row in adGrprows:
-            print(row)
-
     with ui.card().classes('w-full'):
-        with ui.table(columns=AdGrpTableCol, rows=adGrprows).classes('w-full') as aGrptable:
-            aGrptable.add_slot(
-                "body",
-                r"""
-                <q-tr :props="props">
-                    <q-td key="Os_type" :props="props" class="w-8 ellipsis">
-                        {{ props.row.Os_type }}
-                    </q-td>
-                    <q-td key="instance_type" :props="props" class="w-8 ellipsis">
-                        {{ props.row.instance_type }}
-                    </q-td>
-                    <q-td key="server_prefix" :props="props" class="w-8 ellipsis">
-                        {{ props.row.server_prefix }}
-                    </q-td>
-                    <q-td key="Server_sequence_number" :props="props" class="w-8 ellipsis">
-                        {{ props.row.Server_sequence_number }}
-                    </q-td>
-                    <q-td key="Quantity" :props="props" class="w-8 ellipsis">
-                        {{ props.row.Quantity }}
-                    </q-td>
-                    <q-td key="Subnet_information" :props="props" class="w-8 ellipsis">
-                        {{ props.row.Subnet_information }}
-                    </q-td>
-                    <q-td auto-width>
-                        <q-btn size="sm" color="warning" round dense icon="delete" :props="props"
-                            @click="() => $parent.$emit('delete', props.row)">
-                        </q-btn>
-                    </q-td>
-                </q-tr>
-                """,
-            )
-            with aGrptable.add_slot('bottom-row'):
-                with aGrptable.row():
-                    with aGrptable.cell():
-                        os_type_select = ui.select(options=Os_type_option, label='OS Type')
-                    with aGrptable.cell():
-                        instance_type_select = ui.select(options=Instance_type_option, label='Instance Type')
-                    with aGrptable.cell():
-                        server_prefix_input = ui.input(label='Server Prefix')
-                    with aGrptable.cell():
-                        server_seq_num_input = ui.input(label='Server Sequence Number')
-                    with aGrptable.cell():
-                        quantity_input = ui.input(label='Quantity')
-                    with aGrptable.cell():
-                        subnet_info_input = ui.input(label='Subnet Information')
-                    with aGrptable.cell():
-                        ui.button(on_click=lambda: (aGrptable.add_rows({'id': time.time(), 'Os_type': os_type_select.value, 'instance_type': instance_type_select.value, 'server_prefix': server_prefix_input.value, 'Server_sequence_number': server_seq_num_input.value, 'Quantity': quantity_input.value, 'Subnet_information': subnet_info_input.value}),
-                                  os_type_select.set_value(None),
-                                  instance_type_select.set_value(None),
-                                  server_prefix_input.set_value(None),
-                                  server_seq_num_input.set_value(None),
-                                  quantity_input.set_value(None),
-                                  subnet_info_input.set_value(None)
-                                                    ), icon='add')
-                aGrptable.on("delete", deleteGrp)
+        with ui.row().classes('w-full'):
+            serverDetailsTable = ui.aggrid({
+                'defaultColDef': {'flex': 1},
+                'columnDefs': server_details_columns,
+                'rowData': server_details_rows,
+                'rowSelection': 'single'
+            }).classes('w-full')
 
-        ui.button('Save', on_click=print_table_data, icon='save')
+        with ui.row().classes('self-center place-items-center'):
+            ui.button('Add', on_click=serverDetailsDialog.open, icon='add').classes('m-2')
+            ui.button('Edit', on_click=lambda: asyncio.create_task(edit_server_details()), icon='edit').classes('m-2')
+            ui.button('Delete', on_click=lambda: asyncio.create_task(remove_server_details()), icon='delete').classes('m-2')
+            ui.button('Save', on_click=save_all_details, icon='save').classes('m-2')
